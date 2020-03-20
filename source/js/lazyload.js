305b47c4912a66017f63b7d4f26593cb362673f1
limhepro <github2@chralpha.com>
2020-03-20 10:19:19 +0800

    add support of lazyload image

---
+++ source/js/lazyload.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function query(selector) {
+    return Array.from(document.querySelectorAll(selector));
+}
+
+var io = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+            var img = entry.target;
+            img.srcset = img.getAttribute('data-src');
+            img.className += ' loaded'
+            io.unobserve(img);
+        }
+    });
+});
+
+query('img.lazyload').forEach(function (item) {
+    io.observe(item);
+});
